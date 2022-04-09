d4758d8f670e0ad4c45905eda8c895a49d02ce49
huy2012 <huynn@reactplus.jp>
2022-04-09 17:05:14 +0700

    api

--- app.js
+++ app.js
@@ -31,16 +31,16 @@ app.set('view engine', '.html');
 app.set('views', path.join(__dirname, 'views'));
 
 // để post data json
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 // cors
 app.use(cors());
 app.use(morgan('dev'));
 
-// app.use(express.json({ limit: '100mb' }));
-// app.use(express.urlencoded({ limit: '100mb' }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb' }));
 
-app.use(express.limit(100000000))
+// app.use(express.limit(100000000));
 
 // kết nối database
 app.use(myConnection(mysql, {
